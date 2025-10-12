69109491f3a3ecb0a6d478b71f982fbf92c7159f
Arjunliljo <arjun7180@gmail.com>
2025-10-13 01:35:30 +0530

    booking section updations

--- assets/js/booking.js
+++ assets/js/booking.js
@@ -1,27 +1,26 @@
 const editBtn = document.querySelector(".booking-edit-btn");
 const backBtnLeft = document.querySelector(".back-btn-left");
 const navBtn = document.querySelector(".nav-btn");
+const newBookingNextBtn = document.querySelector(".booking-next-btn");
+
+const bookingTable = document.querySelector(".booking-table");
+const bookingDetails = document.querySelector(".booking-details");
+
+const handleBackBtnLeft = () => {};
+
+const handleNewBookingNextBtn = (e) => {};
+
+const handleStartNewBooking = (e) => {};
 
 const handleEditBooking = () => {
-  const bookingTable = document.querySelector(".booking-table");
+  if (bookingTable && bookingDetails) {
+    bookingTable.classList.add("remove");
+    bookingDetails.classList.remove("remove");
+  }
 
-  const bookingDetails = document.querySelector(".booking-details");
-  bookingTable.classList.add("remove");
-  bookingDetails.classList.remove("remove");
-
-  backBtnLeft.classList.remove("remove");
-  navBtn.classList.add("remove");
+  if (backBtnLeft) backBtnLeft.classList.remove("remove");
+  if (navBtn) navBtn.classList.add("remove");
 };
 
-const handleBackBtnLeft = () => {
-  const bookingTable = document.querySelector(".booking-table");
-
-  const bookingDetails = document.querySelector(".booking-details");
-  bookingTable.classList.remove("remove");
-  bookingDetails.classList.add("remove");
-
-  backBtnLeft.classList.add("remove");
-};
-
-editBtn.addEventListener("click", handleEditBooking);
-backBtnLeft.addEventListener("click", handleBackBtnLeft);
+if (editBtn) editBtn.addEventListener("click", handleEditBooking);
+if (backBtnLeft) backBtnLeft.addEventListener("click", handleBackBtnLeft);
